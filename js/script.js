f656ee1104ad9dc887260d09fe5821a8650e0cc6
Alfredo Ficocelli <alfredo.ficocelli@gmail.com>
2021-04-08 15:24:10 +0200

    add. options

--- js/script.js
+++ js/script.js
@@ -6,6 +6,7 @@ var app = new Vue({
 
     albums: [],
 
+    genreOption:[]
 
   },
 
@@ -18,6 +19,19 @@ var app = new Vue({
       myThis.albums = response.data.response;
 
       console.log(myThis.albums);
+
+      for(var i = 0; i < myThis.albums.length; i++){
+
+        if (myThis.genreOption.includes(myThis.albums[i].genre) == false){
+
+          myThis.genreOption.push(myThis.albums[i].genre);
+
+        }
+
+      }
+
+      console.log(myThis.genreOption);
+
     });
 
 
