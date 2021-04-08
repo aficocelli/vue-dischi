cdd5a8a38e6b12e16d32adce74b348be236a8596
Alfredo Ficocelli <alfredo.ficocelli@gmail.com>
2021-04-08 14:24:57 +0200

    add. albums

--- js/script.js
+++ js/script.js
@@ -1,1 +1,27 @@
+var app = new Vue({
 
+  el: '#root',
+
+  data:{
+
+    albums: [],
+
+
+  },
+
+  mounted: function(){
+
+    let myThis = this;
+    axios.get('https://flynn.boolean.careers/exercises/api/array/music')
+    .then(function(response){
+
+      myThis.albums = response.data.response;
+
+      console.log(myThis.albums);
+    });
+
+
+
+
+  }
+});
